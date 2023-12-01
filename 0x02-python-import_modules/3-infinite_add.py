b2bf0fc75a51cea7a91329386a1415a873242fa4
houussame <2015.houdou@gmail.com>
2023-12-01 11:49:44 -0800

    infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    import sys, math
-    result = 0
-    for i in sys.argv:
-        result += int(i)
-        print("{}".format(resul
+    import sys
+
+    somme = 0
+    for i in range(len(sys.argv) - 1):
+        somme += int(sys.argv[i + 1])
+    print(somme)
