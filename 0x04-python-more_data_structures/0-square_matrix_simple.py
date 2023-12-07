87176c1fbf824617d553784e6f09558fc269a62b
houussame <2015.houdou@gmail.com>
2023-12-07 04:12:40 -0800

    square

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return None
+    for new_matrix in matrix:
+        if len(new_matrix) == 0:
+    for i in range(len(new_matrix)):
+        print("{:d}".format(new-matrix[i])
