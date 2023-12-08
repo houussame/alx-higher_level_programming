3226b250a11c745e73c1c881e095eaaa31a8e814
houussame <2015.houdou@gmail.com>
2023-12-08 10:14:34 -0800

    search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = search_replace(my_list, 2, 89)
-    return new_list
+    n_list = list(my_list)
+    for i in range(len(n_list)):
+        if n_list[i] == search:
+            n_list[i] = replace
+    return n_list
