4c4647afac2be5893f7eef2451e4d85cd152dbbd
houussame <2015.houdou@gmail.com>
2023-12-08 10:23:56 -0800

    uniq

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    return list(map(lambda add : list(map(lambda x: x + x, add)), my_list))
-    print("Result: {:d}".format(result))
+    return sum(set(my_list))
