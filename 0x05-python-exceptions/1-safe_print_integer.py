ea9481e4ce543b2e0b30eb049dadcabb5dba428c
houussame <2015.houdou@gmail.com>
2023-12-18 10:43:36 -0800

    safee

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    value = 0
+    try:
+        print("{:d}".format(value))
+        if value == int
+        return True
+    else:
+        return False
+    except IndexError:
+        return value
