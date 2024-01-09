54e9e29a2e8149837b90be5438ccb0a9d15c6112
houussame <2015.houdou@gmail.com>
2024-01-09 10:47:12 -0800

    j

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""Module containing the function read_file"""
+
+
 def read_file(filename=""):
-    with open('UTF8.txt', 'r') as file:
+    """Reads a file and prints to stdout.
+
+    Args:
+        filename (str, optional): name of file to read. Defaults to "".
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
