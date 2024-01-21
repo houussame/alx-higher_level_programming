6a9c0b7d2d770fc5a583b9c9e7879a5cc49dd844
houussame <2015.houdou@gmail.com>
2024-01-21 09:29:37 -0800

    b

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,7 +10,7 @@ class Base:
         '''function'''
 
         if id is not None:
-            self.id == id
+            self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
